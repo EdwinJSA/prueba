ea3140a6de27e6b8e186a695a7c7ed59ca2637e6
CupOfCoffe <edwinjsa2021@gmail.com>
2024-09-24 20:54:44 -0600

    Leccion 1

---
+++ variables.py
@@ -0,0 +1,8 @@
+#Para crear una variable solo se necesita el nombre y el valor
+#hay diferentes tipo de variables. Sus nombres deben empezar en minusculas
+#si son dos palabras se utiliza camelCase(ejemplo: nombreVariable)
+
+nombre = "Carlos" #string
+edad = 23 #int
+sexo = 'M' #char
+vivo = True #bool

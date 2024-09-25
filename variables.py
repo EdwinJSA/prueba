54f3fd3e28f2e9490e914ab036012477d9df3fa3
CupOfCoffe <edwinjsa2021@gmail.com>
2024-09-24 21:09:57 -0600

    Hi

--- variables.py
+++ variables.py
@@ -10,3 +10,10 @@ altura = 1.75 #float
 
 nombre2= input("introduzca su nombre")
 print(nombre2)
+
+#Castear
+edad2 = int(input("introduzca su edad"))
+print(edad2)
+
+print("Variable nombre2: " + type(nombre2))
+print("Variable edad2: " + type(edad2))
